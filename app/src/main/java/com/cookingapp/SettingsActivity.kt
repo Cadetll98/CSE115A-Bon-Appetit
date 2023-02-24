3d22496390dda9ce4ed6a86aee2cf9eea03c2872
yjian115 <yjian115@ucsc.edu>
2023-02-24 14:03:36 -0800

    finish the setting, base on login activity have a logout and set the current user to null, will back to login page

--- app/src/main/java/com/cookingapp/SettingsActivity.kt
+++ app/src/main/java/com/cookingapp/SettingsActivity.kt
@@ -1,10 +1,19 @@
 package com.cookingapp
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
 import androidx.preference.PreferenceFragmentCompat
+import com.cookingapp.ui.theme.*
+
 
 class SettingsActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +24,28 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
+        supportActionBar?.title = "Settings"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+        private val key1 = "mode"
+        private var preferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener? =
+            null
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            preferenceChangeListener =
+                OnSharedPreferenceChangeListener { sharedPreferences, key ->
+                    if (key == key1) {
+
+                    }
+                }
         }
+
     }
 }
